949d10f8981c6f55df4ee1ac1828eb8dba1cafae
antonio.bustos <antonio.bustos@sigames.com>
2020-12-04 13:26:53 +0000

    Using \b is better

--- exercise4/exercise4.py
+++ exercise4/exercise4.py
@@ -3,13 +3,13 @@ import re
 
 
 VALID_FIELDS = ("ecl:", "pid:", "eyr:", "hcl:", "byr:", "iyr:", "hgt:")
-REGEX = ("byr:19[2-9][0-9]|200[0-2](\\s|$)",
-         "iyr:20(1[0-9]|20)(\\s|$)",
-         "eyr:20(2[0-9]|30)(\\s|$)",
-         "hgt:((1([5-8][0-9]|9[0-3])cm)|(59|6[0-9]|7[0-6])in)(\\s|$)",
-         "hcl:#([0-9a-f]{6})(\\s|$)",
-         "ecl:(amb|blu|brn|gry|grn|hzl|oth)(\\s|$)",
-         "pid:([0-9]{9})(\\s|$)")
+REGEX = ("byr:19[2-9][0-9]|200[0-2]\\b",
+         "iyr:20(1[0-9]|20)\\b",
+         "eyr:20(2[0-9]|30)\\b",
+         "hgt:((1([5-8][0-9]|9[0-3])cm)|(59|6[0-9]|7[0-6])in)\\b",
+         "hcl:#([0-9a-f]{6})\\b",
+         "ecl:(amb|blu|brn|gry|grn|hzl|oth)\\b",
+         "pid:([0-9]{9})\\b")
 
 
 def read_file(filename: str) -> List[str]:
