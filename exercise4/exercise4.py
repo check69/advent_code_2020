8b69cee82dd222eb58b24e112ab593bc498808f5
antonio.bustos <antonio.bustos@sigames.com>
2020-12-04 12:27:29 +0000

    Improve a little bit efficiency avoiding creating valid_fields every single time

--- exercise4/exercise4.py
+++ exercise4/exercise4.py
@@ -2,24 +2,23 @@ from typing import List
 import re
 
 
+VALID_FIELDS = ("ecl:", "pid:", "eyr:", "hcl:", "byr:", "iyr:", "hgt:")
+REGEX = ("byr:19[2-9][0-9]|200[0-2]",
+         "iyr:20(1[0-9]|20)",
+         "eyr:20(2[0-9]|30)",
+         "hgt:((1([5-8][0-9]|9[0-3])cm)|(59|6[0-9]|7[0-6])in)",
+         "hcl:#([0-9a-f]{6})",
+         "ecl:(amb|blu|brn|gry|grn|hzl|oth)",
+         "pid:([0-9]{9})(\\s|$)")
+
+
 def read_file(filename: str) -> List[str]:
     with open(filename, "r") as file:
         return file.read().split("\n\n")
 
 
 def is_valid_passport(passport: str) -> bool:
-    valid_fields = ("ecl:", "pid:", "eyr:", "hcl:", "byr:", "iyr:", "hgt:")
-    return all(field in passport for field in valid_fields)
-
-
-byr = "byr:19[2-9][0-9]|200[0-2]"
-iyr = "iyr:20(1[0-9]|20)"
-eyr = "eyr:20(2[0-9]|30)"
-hgt = "hgt:((1([5-8][0-9]|9[0-3])cm)|(59|6[0-9]|7[0-6])in)"
-hcl = "hcl:#([0-9a-f]{6})"
-ecl = "ecl:(amb|blu|brn|gry|grn|hzl|oth)"
-pid = "pid:([0-9]{9})(\\s|$)"
-REGEX = (byr, iyr, eyr, hgt, hcl, ecl, pid)
+    return all(field in passport for field in VALID_FIELDS)
 
 
 def is_fields_valid(passport: str) -> bool:
